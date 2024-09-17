a7b1058d05b40cff51ac738e0069e97ab5dc3ddb
mahadanso <mahadanso79@gmail.com>
2024-09-17 10:55:01 +0200

    added head pose estimation

--- src/facedetection.py
+++ src/facedetection.py
@@ -2,12 +2,14 @@
 
 import rospy
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import cv2
+from cv_bridge import CvBridge
 from facenet_pytorch import MTCNN
 import numpy as np
+import mediapipe as mp
 from collections import OrderedDict
 from face_detection.msg import FaceDetectionData
+# from google.colab.patches import cv2_imshow
 
 # from l2cs import Pipeline, render
 # import cv2, torch
@@ -25,6 +27,14 @@ class FaceDetectionNode:
 
         # Create a publisher to publish processed images
         self.data_pub = rospy.Publisher('faceDetection/data', FaceDetectionData, queue_size=10)
+
+        # Initialize face mesh
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = self.mp_face_mesh.FaceMesh(min_detection_confidence=0.5,min_tracking_confidence=0.5)
+
+        self.mp_drawing = mp.solutions.drawing_utils
+
+        self.drawing_spec = self.mp_drawing.DrawingSpec(color=(128,0,128),thickness=2,circle_radius=1)
 
         # Initialize tracker
         self.next_object_id = 0
@@ -96,13 +106,95 @@ class FaceDetectionNode:
         return np.linalg.norm(a[:, np.newaxis] - b, axis=2)
 
     def image_callback(self, data):
-        # print("callback")
         # Convert ROS Image message to OpenCV image
         cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
 
+        image = cv2.cvtColor(cv2.flip(cv_image,1),cv2.COLOR_BGR2RGB) #flipped for selfie view
+
+        image.flags.writeable = False
+
+        results = self.face_mesh.process(image)
+
+        image.flags.writeable = True
+
+        image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+
+        img_h , img_w, img_c = image.shape
+        face_2d = []
+        face_3d = []
+
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                for idx, lm in enumerate(face_landmarks.landmark):
+                    if idx == 33 or idx == 263 or idx ==1 or idx == 61 or idx == 291 or idx==199:
+                        if idx ==1:
+                            nose_2d = (lm.x * img_w,lm.y * img_h)
+                            nose_3d = (lm.x * img_w,lm.y * img_h,lm.z * 3000)
+                        x,y = int(lm.x * img_w),int(lm.y * img_h)
+
+                        face_2d.append([x,y])
+                        face_3d.append(([x,y,lm.z]))
+
+
+                #Get 2d Coord
+                face_2d = np.array(face_2d,dtype=np.float64)
+
+                face_3d = np.array(face_3d,dtype=np.float64)
+
+                focal_length = 1 * img_w
+
+                cam_matrix = np.array([[focal_length,0,img_h/2],
+                                    [0,focal_length,img_w/2],
+                                    [0,0,1]])
+                distortion_matrix = np.zeros((4,1),dtype=np.float64)
+
+                success,rotation_vec,translation_vec = cv2.solvePnP(face_3d,face_2d,cam_matrix,distortion_matrix)
+
+
+                #getting rotational of face
+                rmat,jac = cv2.Rodrigues(rotation_vec)
+
+                angles,mtxR,mtxQ,Qx,Qy,Qz = cv2.RQDecomp3x3(rmat)
+
+                x = angles[0] * 360
+                y = angles[1] * 360
+                z = angles[2] * 360
+
+                #here based on axis rot angle is calculated
+                if y < -10:
+                    text="Looking Left"
+                elif y > 10:
+                    text="Looking Right"
+                elif x < -10:
+                    text="Looking Down"
+                elif x > 10:
+                    text="Looking Up"
+                else:
+                    text="Forward"
+
+                nose_3d_projection,jacobian = cv2.projectPoints(nose_3d,rotation_vec,translation_vec,cam_matrix,distortion_matrix)
+
+                p1 = (int(nose_2d[0]),int(nose_2d[1]))
+                p2 = (int(nose_2d[0] + y*10), int(nose_2d[1] -x *10))
+
+                cv2.line(image,p1,p2,(255,0,0),3)
+
+                cv2.putText(image,text,(20,50),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),2)
+                cv2.putText(image,"x: " + str(np.round(x,2)),(500,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+                cv2.putText(image,"y: "+ str(np.round(y,2)),(500,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+                cv2.putText(image,"z: "+ str(np.round(z, 2)), (500, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
+            self.mp_drawing.draw_landmarks(image=image,
+                                    landmark_list=face_landmarks,
+                                    connections=self.mp_face_mesh.FACEMESH_CONTOURS,
+                                    landmark_drawing_spec=self.drawing_spec,
+                                    connection_drawing_spec=self.drawing_spec)
+
+        cv2.imshow('Head Pose Detection',image)
+
         # Perform face detection
         boxes, probs, landmarks = self.detector.detect(cv_image, landmarks=True)
-        # print(boxes)
 
         # Prepare centroids for tracking
         input_centroids = []
@@ -153,7 +245,8 @@ class FaceDetectionNode:
         cv2.waitKey(1)
 
         # Convert OpenCV image back to ROS Image message
-        processed_image_msg = self.bridge.cv2_to_imgmsg(cv_image, 'bgr8')
+        # processed_image_msg = self.bridge.cv2_to_imgmsg(cv_image, 'bgr8')
+        print("call back end...")
 
 
         # Publish the processed image
